14a3c13ff40c862fe32b0c6f1de4233c084eedd1
madderlake <mary@maryahayne.com>
2023-11-13 13:49:04 -0800

    fix dir

--- server.ts
+++ server.ts
@@ -35,7 +35,7 @@ app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, '/dist')));
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 export const io = new Server<
   ClientToServerEvents,
