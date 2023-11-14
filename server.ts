b588a7865b16e0dfc30e8b437cb23b285a3672c0
Mary Hayne <mary@maryahayne.com>
2023-11-14 07:07:54 -0800

    change root (#18)

--- server.ts
+++ server.ts
@@ -37,12 +37,12 @@ app.use(cors());
 app.use(express.static(path.resolve(__dirname, './dist')));
 
 // Handle GET requests to /api route
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from server!' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Hello from server!' });
+// });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
 });
 
