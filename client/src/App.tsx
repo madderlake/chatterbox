cf7331b3fe8636547fcd93a23e5087c184bfb535
madderlake <mary@maryahayne.com>
2023-11-20 11:48:41 -0800

    set dev env vars

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Routes from './components/Router';
 
-import { ClientContext, client } from './contexts/ClientContext';
+import { ClientContext } from './contexts/ClientContext';
+import { socket } from './contexts/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { persistStore } from 'redux-persist';
@@ -13,7 +14,7 @@ const persistor = persistStore(store);
 const App = () => {
   return (
     <div className="App">
-      <ClientContext.Provider value={client}>
+      <ClientContext.Provider value={socket}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <Routes />
