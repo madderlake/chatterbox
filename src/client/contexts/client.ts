b6b635f9174c23e23729f6909e7063818ff913a2
Mary Hayne <mary@maryahayne.com>
2023-12-04 22:50:57 -0800

    Fix ports (#42)
    * fix build and deploy
    
    * fix client url, add transpile only etc
    
    * update start script to server start

--- src/client/contexts/client.ts
+++ src/client/contexts/client.ts
@@ -1,7 +1,6 @@
 import io from 'socket.io-client';
 
-const socketUrl =
-  process.env.REACT_APP_BACKEND_URL + ':' + process.env.REACT_APP_SERVER_PORT;
+const socketUrl = `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_SERVER_PORT}`;
 
 export const socket =
   socketUrl !== undefined &&
--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,12 @@ import * as path from 'path';
 import { Server } from 'socket.io';
 
 //Types
-import type { User, Message } from '../../types';
-import StartListeners from './utils/listeners';
+import type { User, Message } from '../types';
+import StartListeners from './server/utils/listeners';
 
 const app = express();
 const httpServer = http.createServer(app);
-const PORT = process.env.REACT_APP_SERVER_PORT || process.env.PORT;
+const PORT = process.env.REACT_APP_SERVER_PORT || process.env.PORT || 8083;
 
 type Data = User | Message;
 type BasicEmit = (data: Data | Data[]) => void;
@@ -38,9 +38,8 @@ const corsOptions = {
   methods: ['GET', 'OPTIONS'],
 };
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, './dist')));
+app.use(express.static(path.resolve(__dirname, '../dist')));
 
-app.set('port', PORT);
 // Handle GET requests to /api route
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from local server!' });
@@ -48,11 +47,9 @@ app.get('/', (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
   res.writeHead(200, {
     'Content-Type': 'text',
-    // NOTE: you should not use a wildcard CORS config in production.
-    // configure this properly for your needs.
     'Access-Control-Allow-Origin': corsOptions.origin,
     'Access-Control-Allow-Methods': corsOptions.methods,
     'Access-Control-Max-Age': 2592000,
