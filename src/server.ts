cba0626fb1edcacfac6a015a785a6976ff548bf1
Mary Hayne <mary@maryahayne.com>
2024-10-27 16:58:20 -0700

    fix ports, for now (#68)

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ import StartListeners from "./server/listeners";
 
 const app = express();
 const httpServer = http.createServer(app);
-const PORT = process.env.VITE_SERVER_PORT || process.env.PORT || 8083;
+const PORT = 8000;
 
 type Data = User | Message;
 type BasicEmit = (data: Data | Data[]) => void;
